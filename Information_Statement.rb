3cbba56bd56ff9118723bb261d2d45d93d32159b
Celine <62265459+CelineRondeau@users.noreply.github.com>
2020-03-20 12:51:00 -0400

    Information_Statement.rb
    Creating a personal statement for Ruby to say about yourself.

---
+++ Information_Statement.rb
@@ -0,0 +1,15 @@
+first_name = 'tasha'.chomp
+first_name.capitalize!
+puts "Your name is #{first_name}!"
+
+last_name = 'richard'.chomp
+last_name.capitalize!
+puts "Your last name is #{last_name}!"
+
+city = 'vancouver'.chomp
+city.capitalize!
+puts "Your city is #{city}!"
+
+province = 'bc'.chomp.upcase
+province.upcase!
+print "Your province is #{province}."
